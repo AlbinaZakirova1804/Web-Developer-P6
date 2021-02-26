48bf44ec9470868caee69870ef5b0bf2de91f08a
albina Zakirova <albinazakirova@albinas-Air.home>
2021-02-26 16:26:53 -0500

    added cross origin sharing code to app.js

--- backend/app.js
+++ backend/app.js
@@ -21,5 +21,12 @@ app.use((req, res, next) => {
     console.log("Response sent successfully!");
 })
 
+//Cross Origin Resource Sharing
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
 
 module.exports = app;
